1a4f906cff550fb8205ae046aeb6e746ea4ec402
MathisB2 <mathis.bodin2@gmail.com>
2024-09-16 12:23:47 +0200

    login launch from main

--- app/src/main/java/com/mobcoin/app/MainActivity.kt
+++ app/src/main/java/com/mobcoin/app/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.mobcoin.app
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -29,7 +30,11 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
