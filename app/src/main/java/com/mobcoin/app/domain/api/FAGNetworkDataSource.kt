2ad15ba420d93ee33ee04648fdb6848003d69758
maxence caron <maxence.caron04@gmail.com>
2024-09-20 17:33:08 +0200

    start fear and greed index retrofit

---
+++ app/src/main/java/com/mobcoin/app/domain/api/FAGNetworkDataSource.kt
@@ -0,0 +1,33 @@
+package com.mobcoin.app.domain.api
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import timber.log.Timber
+
+object FAGNetworkDataSource {
+    private const val BASE_URL = "https://api.alternative.me/"
+
+
+    private val logInterceptor = HttpLoggingInterceptor { message ->
+        Timber.tag(
+            "OkHttp"
+        ).d(message)
+    }.apply { level = HttpLoggingInterceptor.Level.BODY }
+
+    private val okHttp = OkHttpClient.Builder()
+        .addNetworkInterceptor(logInterceptor)
+        .build()
+
+
+    private val retrofit =
+        Retrofit.Builder()
+            .client(okHttp)
+            .baseUrl(FAGNetworkDataSource.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+
+    val apiService = retrofit.create(FAGService::class.java)
+}
