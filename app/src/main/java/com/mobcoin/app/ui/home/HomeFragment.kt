24251531744991dfae037d3bb2624736cdf57b57
maxence caron <maxence.caron04@gmail.com>
2024-10-09 17:04:45 +0200

    implémentation fragment coinList sur home

--- app/src/main/java/com/mobcoin/app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mobcoin/app/ui/home/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.mobcoin.app.adapter.CoinItemAdapter
 import com.mobcoin.app.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -28,10 +29,17 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val recyclerView = binding.recyclerViewCoinList
+        val adapter = CoinItemAdapter(requireContext(), homeViewModel.coins.value ?:  emptyList())
+
+        homeViewModel.coins.observe(viewLifecycleOwner){
+            adapter.setDataset(it ?: emptyList())
         }
+
+        recyclerView.adapter = adapter
+        homeViewModel.fetchCoins()
+
+
         return root
     }
 
--- app/src/main/java/com/mobcoin/app/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/mobcoin/app/ui/home/HomeViewModel.kt
@@ -5,21 +5,22 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mobcoin.app.domain.FNGRepository
+import com.mobcoin.app.domain.GeckoRepository
+import com.mobcoin.app.domain.httpQuery.MarketCoinsQuery
+import com.mobcoin.app.model.Coin
 import com.mobcoin.app.model.FNG
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
+import retrofit2.Response
 import timber.log.Timber
 
 class HomeViewModel : ViewModel() {
 
     private var _fng : MutableLiveData<FNG?> = MutableLiveData()
-
     val fng : LiveData<FNG?> = _fng
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
-    }
-    val text: LiveData<String> = _text
+    private var _coins : MutableLiveData<List<Coin>?> = MutableLiveData()
+    val coins : LiveData<List<Coin>?> = _coins
 
     fun fetchFNG() {
         viewModelScope.launch {
@@ -33,6 +34,18 @@ class HomeViewModel : ViewModel() {
                     if(!data.isNullOrEmpty()){
                         _fng.postValue(it.body()?.data?.first())
                     }
+                }
+        }
+    }
+
+    fun fetchCoins() {
+        viewModelScope.launch {
+            GeckoRepository.getCoins(MarketCoinsQuery())
+                .catch {
+                    Timber.e(it)
+                }
+                .collect {
+                    _coins.postValue(it.body())
                 }
         }
     }
