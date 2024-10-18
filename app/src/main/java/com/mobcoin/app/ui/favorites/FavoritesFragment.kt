b010380c6e76ba6e43ce9280cc27d693517fc7e5
MathisB2 <mathis.bodin2@gmail.com>
2024-10-18 10:59:19 +0200

    added loggedOut fragment to favorites

--- app/src/main/java/com/mobcoin/app/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/mobcoin/app/ui/favorites/FavoritesFragment.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.mobcoin.app.databinding.FragmentFavoritesBinding
+import com.mobcoin.app.ui.others.LoggedOutFragment
 
 class FavoritesFragment : Fragment(){
 
@@ -20,16 +21,24 @@ class FavoritesFragment : Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val favoritesViewModel =
-            ViewModelProvider(this).get(FavoritesViewModel::class.java)
+        val favoritesViewModel = ViewModelProvider(this).get(FavoritesViewModel::class.java)
 
         _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textFavorites
-        favoritesViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+
+
+
+        var logged: Boolean = false
+
+        if(logged){
+//            childFragmentManager.beginTransaction().replace(fragmentId, LoggedOutFragment()).commit() //todo: 
+        }else{
+            childFragmentManager.beginTransaction().replace(binding.favoritesFragmentContainer.id, LoggedOutFragment()).commit()
         }
+
+
+
         return root
     }
 
