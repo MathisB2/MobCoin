074da7c5913dd61ae8f23cd0adbbba437e65b4b6
MathisB2 <mathis.bodin2@gmail.com>
2024-11-06 14:15:57 +0100

    home view ui changes

--- app/src/main/java/com/mobcoin/app/MainActivity.kt
+++ app/src/main/java/com/mobcoin/app/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.mobcoin.app
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -13,7 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
