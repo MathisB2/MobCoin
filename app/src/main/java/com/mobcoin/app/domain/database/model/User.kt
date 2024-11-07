0f1534f0afd23eb6fbf47ce625c60390f7cb7497
MathisB2 <mathis.bodin2@gmail.com>
2024-11-07 14:12:00 +0100

    userId auto increment

--- app/src/main/java/com/mobcoin/app/domain/database/model/User.kt
+++ app/src/main/java/com/mobcoin/app/domain/database/model/User.kt
@@ -7,7 +7,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "t_user", indices = [Index(value = ["email"], unique = true, )])
 data class User (
     @PrimaryKey(autoGenerate = true)
-    var id: Long,
+    var id: Long = 0,
 
     val surname: String,
     val email: String,
