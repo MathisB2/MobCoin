c976880e4d07b0d135d62675c6d85cd2506c5dcc
MathisB2 <mathis.bodin2@gmail.com>
2024-11-16 18:34:56 +0100

    fix for app crashing when offline

--- app/src/main/java/com/mobcoin/app/ui/favorites/ConnectedFavoritesViewModel.kt
+++ app/src/main/java/com/mobcoin/app/ui/favorites/ConnectedFavoritesViewModel.kt
@@ -1,27 +1,37 @@
 package com.mobcoin.app.ui.favorites
 
 import android.content.Context
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.mobcoin.app.domain.FavoriteRepository
 import com.mobcoin.app.domain.GeckoRepository
-import com.mobcoin.app.model.Coin
 import com.mobcoin.app.model.DetailedCoin
-import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 
-class ConnectedFavoritesViewModel() : ViewModel() {
+class ConnectedFavoritesViewModel : ViewModel() {
     fun getFavoriteCoins(context: Context): LiveData<List<DetailedCoin>> {
         val liveData = MutableLiveData<List<DetailedCoin>>()
 
         viewModelScope.launch {
             FavoriteRepository.getFavorites(context).collect {
-                val listCoins = it.map{ coinData ->
-                    GeckoRepository.getCoinById(coinData.name).first().body()!!
+                val listCoins = ArrayList<DetailedCoin>()
 
+                it.forEach { coinData ->
+                    val response = GeckoRepository.getCoinById(coinData.geckoId)
+                    response.catch { error ->
+                        Log.e("ConnectedFavoritesViewModel", error.message.toString())
+                    }.collect{ response ->
+                        if (response.isSuccessful)
+                            response.body()?.let { detailedCoin ->
+                                listCoins.add(detailedCoin)
+                            }
+                        else
+                            Log.e("ConnectedFavoritesViewModel", "Error fetching coin data")
+                    }
                 }
                 liveData.postValue(listCoins)
             }
