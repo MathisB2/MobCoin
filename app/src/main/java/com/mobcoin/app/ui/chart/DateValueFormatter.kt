da783bb72844b59372b527dea6df5ff71d0065aa
MathisB2 <mathis.bodin2@gmail.com>
2024-11-30 17:39:05 +0100

    fixed wrong value formatter

--- app/src/main/java/com/mobcoin/app/ui/chart/DateValueFormatter.kt
+++ app/src/main/java/com/mobcoin/app/ui/chart/DateValueFormatter.kt
@@ -1,17 +1,15 @@
 package com.mobcoin.app.ui.chart
 
-import com.github.mikephil.charting.components.AxisBase
 import com.github.mikephil.charting.formatter.ValueFormatter
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
-import java.util.concurrent.TimeUnit
 
 class DateValueFormatter: ValueFormatter() {
-    private val mFormat = SimpleDateFormat("HH:mm", Locale.ENGLISH)
+    private val mFormat = SimpleDateFormat("dd/MM", Locale.ENGLISH)
 
     override fun getFormattedValue(value: Float): String {
-        val millis = TimeUnit.HOURS.toMillis(value.toLong())
+        val millis = value.toLong()
         return mFormat.format(Date(millis))
     }
 }
